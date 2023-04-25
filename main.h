bcaaf59406ea43dfbba0b97a548da165b2a957f0
Evans Mukonyole <emukonyole@gmail.com>
2023-04-24 22:24:32 -0700

    Main.h file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+/*
+ * _putchar prototype
+ * _printf prototype
+ */
+
+int _Putchar(char c);
+int _printf(const char *format, ...);
+
+#endif

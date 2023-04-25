0b6d1087b7d756a77a9c64db1365f383d80f16bd
Felix Sindani <felixsindani72@gmail.com>
2023-04-25 12:32:29 -0700

    Fix bug

--- _printf.c
+++ _printf.c
@@ -11,40 +11,39 @@ int _printf(const char *format, ...)
 	int count, a, b, c;
 	char *s;
 	count = 0;
-
-	va_list var_name;
-	va_start(var_name, format);
+	va_list arg;
+	va_start(arg, format);
 
 	for (a = 0; format[a] != '\0'; a++)
 	{
 		if (format[a] != '%')
-			_putchar(format[a]);
-		else if (format[a + 1] == 'c');
+			putchar(format[a]);
+		else if (format[a + 1] == 'c')
 		{
-			c = va_arg(var_name, int);
-			_putchar(c);
+			c = va_arg(arg, int);
+			putchar(c);
+			count++;
 			a++;
 		}
 		else if (format[a + 1] == 's')
 		{
-			s = va_arg(var_name, char *);
+			s = va_arg(arg, char *);
 			a++;
 			b = 0;
 
 			while (s[b] != '\0')
 			{
-				_putchar([b]);
+				putchar([b]);
 				b++;
 				count++;
 			}
 		}
 		else if (format[a + 1] == '%')
 		{
-			_putchar('%');
+			putchar('%');
+			count++;
 			a++;
 		}
-		count++;
 	}
-	count--;
 	return (count);
 }

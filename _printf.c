c6294fb8670a3196666e5523efe368415b5b7f1f
Felix Sindani <felixsindani72@gmail.com>
2023-04-25 15:50:06 -0700

    Fix output

--- _printf.c
+++ _printf.c
@@ -1,22 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
-
 /*
- * _printf - function that prints on stdio
- * @format: format
- * Return: count
- */
-
+* _printf - function that prints on stdio
+* @format: format
+* Return: count
+*/
 int _printf(const char *format, ...)
 {
 	unsigned int a, b, c;
 	int count = 0;
 	char *s;
-	
 	va_list var_name;
+	
 	va_start(var_name, format);
-
 	for (a = 0; format[a] != '\0'; a++)
 	{
 		if (format[a] != '%')
@@ -26,29 +21,26 @@ int _printf(const char *format, ...)
 			c = va_arg(var_name, int);
 			_putchar(c);
 			count++;
-			a++; 
+			a++;
 		}
 		else if (format[a + 1] == 's')
 		{
 			s = va_arg(var_name, char *);
 			a++;
-			b = 0; 
-
+			b = 0;
 			while (s[b] != '\0')
 			{
 				_putchar(s[b]);
 				b++;
 				count++;
-
 			}
-		} 
+		}
 		else if (format[a + 1] == '%')
 		{
 			_putchar('%');
 			count++;
 			a++;
 		}
-
 	}
 	return (count);
 }
